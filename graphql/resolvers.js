4ed4ab8557b10b82b82ad90d9e98448b76d00ffa
chris-tim <christim43@gmail.com>
2022-03-02 20:35:51 +0900

    Mutation addMovie

---
+++ graphql/resolvers.js
@@ -0,0 +1,15 @@
+import { getMovies, getById, deleteMovie, addMovie } from "./db";
+
+
+const resolvers = {
+    Query: {
+        movies:() => getMovies(),
+        movie:(_,{id}) => getById(id)
+    },
+    Mutation: {
+        addMovies:(_,{name,score})=>addMovie(name,score),
+        deleteMovie:(_,{id})=>deleteMovie(id)
+    }
+};
+
+export default resolvers;
