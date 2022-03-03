844126fe6a782670f97eee130141a917740a503d
chris-tim <christim43@gmail.com>
2022-03-03 19:03:38 +0900

    final

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,9 @@
-import { getMovies, getById, deleteMovie, addMovie } from "./db";
+import { getMovies} from "./db";
 
 
 const resolvers = {
     Query: {
-        movies:() => getMovies(),
-        movie:(_,{id}) => getById(id)
-    },
-    Mutation: {
-        addMovies:(_,{name,score})=>addMovie(name,score),
-        deleteMovie:(_,{id})=>deleteMovie(id)
+        movies:(_,{limit,rating}) => getMovies(limit,rating)
     }
 };
 
