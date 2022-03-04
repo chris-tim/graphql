f00fe0b6d6a000319b05b4e71e08e7735ac6dc6e
chris-tim <christim43@gmail.com>
2022-03-04 20:09:25 +0900

    nico version

--- graphql/db.js
+++ graphql/db.js
@@ -1,66 +1,45 @@
-// export let movies = [
-//     {
-//         id:0,
-//         name : "star Wars",
-//         score:1
-//     },
-//     {
-//         id:1,
-//         name : "Avengers",
-//         score:81
-//     },
-//     {
-//         id:2,
-//         name : "The Godfather I",
-//         score:99
-//     },
-//     {
-//         id:3,
-//         name : "Logan",
-//         score:2
-//     }
-// ];
+import axios from "axios";
+const BASE_URL = "https://yts-proxy.now.sh/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-// export const getMovies = () => movies;
-
-// export const getById = id => {
-//     const filteredMovies = movies.filter((movie)=> movie.id === id);
-//     return filteredMovies[0];
-// };
-
-// export const deleteMovie = id => {
-//     const cleanedMovies = movies.filter((movie) => movie.id !== id);
-//     if(movies.length > cleanedMovies.length){
-//         movies = cleanedMovies;
-//         return true;
-//     }else {
-//         return false;
-//     }
-// };
-
-// export const addMovie = (name, score) => {
-//     const newMovie = {
-//         id: `${movies.length + 1}`,
-//         name,
-//         score
-//     };
-//     movies.push(newMovie);
-//     return newMovie;
-// };
-
-import fetch from "cross-fetch";
-
-const API_URL = "https://yts.mx/api/v2/list_movies.json?";
-
-export const getMovies = (limit, rating) =>  {
-    let REQUEST_URL = API_URL;
-    if(limit > 0)   {
-        REQUEST_URL += `limit=${limit}`
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
     }
-    if(rating > 0) {
-        REQUEST_URL += `&minimum_rating=${rating}`
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
     }
-   return fetch(REQUEST_URL)
-   .then(res => res.json())
-   .then(json=>json.data.movies);
-}
+  });
+  return movies;
+};
+
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+};
